703d35fa86819492e6f8dad10674748d121039a8
cele <celeweidmann@gmail.com>
2013-04-08 17:15:10 -0300

    pagina principal

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/comun.css"/>
+    <link rel="shortcut icon" href="favicon.ico">
+</head>
+<body>
+    <div class="hidden-phone">
+    	<div class="row superior"></div>
+        <div class="row medio">
+            <p class="texto">Estamos actualizando el sitio </br></br>Mientras tanto </br></br>Date una vuelta por nuestro</br></br>
+                <a href="http://www.facebook.com/colectivolibre">Facebook</a> y <a href="https://twitter.com/ColectivoLibre">Twitter</a>
+            </p>
+        </div>
+        <div class="row inferiorA"></div>
+        <div class="row inferiorB">
+            <p class="textoContacto"><a href="mailto:contacto@colectivolibre.com.ar">contacto@colectivolibre.com.ar</a></p>
+        </div>
+    </div>
+    <div class="visible-phone">
+        <div class="row">
+            <img src="img/phone/colectivo.png" class="imagen-phone">
+        </div>
+        <div class="row">
+            <p class="textoPhone">Estamos actualizando el sitio, mientras tanto date una vuelta por nuestro
+               <a href="http://www.facebook.com/colectivolibre">Facebook</a> y <a href="https://twitter.com/ColectivoLibre">Twitter</a> </p>
+        </div>
+        <div class="row">
+            <img src="img/phone/hola.png" />
+            <p class="textoPhone"><a href="mailto:contacto@colectivolibre.com.ar">contacto@colectivolibre.com.ar</a></p>
+        </div>
+        <div><iframe class="video" src="http://www.youtube.com/embed/32jiDgQmYPs" allowfullscreen></iframe></div>
+    </div>
+</body>
+</html>
